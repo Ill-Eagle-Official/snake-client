6f7b7ddb8fa75c8f38db42dad3f969fc528490fb
Alexander Jackson <alexx.j.jackson@gmail.com>
2022-08-03 21:30:15 +0000

    added messages, movement keys, and a constants file

---
+++ constants.js
@@ -0,0 +1,21 @@
+const IP = "165.227.47.243";
+const PORT = 50541;
+
+const MOVEUP = 'w';
+const MOVEDOWN = 's';
+const MOVELEFT = 'a';
+const MOVERIGHT = 'd';
+const MESSAGES = {
+  'q': 'Say: snakes... why did it have to be snakes...',
+  'e': 'Say: im tired of these muthalovin snakes on this muthalovin plane!!'
+}
+
+module.exports = {
+  PORT,
+  IP,
+  MOVEUP,
+  MOVEDOWN,
+  MOVELEFT,
+  MOVERIGHT,
+  MESSAGES,
+};
--- input.js
+++ input.js
@@ -1,12 +1,30 @@
+const { MOVEUP, MOVEDOWN, MOVELEFT, MOVERIGHT, MESSAGES } = require('./constants');
 
+let connection;
 
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
-  };
+  }
+  if (key === MOVEUP) {
+    connection.write('Move: up')
+  }
+  if (key === MOVEDOWN) {
+    connection.write('Move: down')
+  }
+  if (key === MOVELEFT) {
+    connection.write('Move: left')
+  }
+  if (key === MOVERIGHT) {
+    connection.write('Move: right');
+  }
+  if(MESSAGES[key]) {
+    connection.write(MESSAGES[key])
+  }
 };
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
