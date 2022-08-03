bc4465c292df12bd115d57216ca774c81f514996
Alexander Jackson <alexx.j.jackson@gmail.com>
2022-08-03 20:58:27 +0000

    added input file, moved functions to there

--- play.js
+++ play.js
@@ -1,21 +1,6 @@
 
-const connect = require('./client.js')
-
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  };
-};
-
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+const connect = require('./client.js');
+const setupInput = require('./input.js');
 
 
 
